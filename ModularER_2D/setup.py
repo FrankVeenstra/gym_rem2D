19b92820e58e92730a32f10f09a234742a7fabd3
FrankVeenstra <f.veenstra00@gmail.com>
2020-10-21 11:26:01 +0200

    Update setup.py
    Updated setup and removed the ø

--- ModularER_2D/setup.py
+++ ModularER_2D/setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='LSystem',
-      description="Modular robotics environment for OpenAi-Gym",
+setup(name='Modular_robtics_2D',
+      description="Modular robotics 2D environment for OpenAi-Gym",
       version='0.1.0',
       keywords="modular robotics gym openai-gym",
-      author="Jørgen Nordmoen and Frank Veenstra",
-      author_email="jorgehn@ifi.uio.no",
+      author="Frank Veenstra and Joergen Nordmoen",
+      author_email="frankvee@uio.no",
       include_package_data=True,
       install_requires=['gym>=0.15', 'pybullet>=2.5', 'numpy>=1.17'],
       test_suite='tests')
